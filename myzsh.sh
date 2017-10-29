8ba2134cc6eb507d1f6ad8ea115103422c382ad5
Bharat Bhushan <bb14521@gmail.com>
2017-10-29 20:24:08 +0530

    update
    update ! !

--- myzsh.sh
+++ myzsh.sh
@@ -6,3 +6,16 @@ fi
 fpath=($ZSH/functions $ZSH/completions $fpath)
 
 autoload -U compaudit compinit
+: ${ZSH_DISABLE_COMPFIX:=true}
+
+if [[ -z "$ZSH_CUSTOM" ]]; then
+    ZSH_CUSTOM="$ZSH/custom"
+fi
+if [[ -z "$ZSH_CACHE_DIR" ]]; then
+  ZSH_CACHE_DIR="$ZSH/cache"
+fi
+for config_file ($ZSH/lib/*.zsh); do
+  custom_config_file="${ZSH_CUSTOM}/lib/${config_file:t}"
+  [ -f "${custom_config_file}" ] && config_file=${custom_config_file}
+  source $config_file
+done
